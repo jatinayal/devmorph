aa6834b450a3ceb5d25ca514a0f7c298c16c29ae
jatinayal <jatinsingh89234@gmail.com>
2026-02-02 19:35:51 +0530

    updated routing logic

--- server-js/server.js
+++ server-js/server.js
@@ -50,11 +50,11 @@ app.use("/api/admin", adminRouter);
 const __dirname = path.resolve();
 
 // serve React build
-app.use(express.static(path.join(__dirname, "client-js/dist")));
+app.use(express.static(path.join(__dirname, "../client-js/dist")));
 
 // React Router fallback — Express 5 safe
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "client-js/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../client-js/dist/index.html"));
 });
 
 
