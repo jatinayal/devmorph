8d4700fd98e77cb035c01259a83a05aa8dfc4a8c
jatinayal <jatinsingh89234@gmail.com>
2026-02-13 00:14:11 +0530

    Update admin login API endpoint to production URL

--- client-js/src/pages/adminlogin.jsx
+++ client-js/src/pages/adminlogin.jsx
@@ -15,7 +15,7 @@ const AdminLogin = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/api/admin/login", {
+      const res = await fetch("https://devmorphs.onrender.com/api/admin/login", {
         method: "POST",
         credentials: "include",
         headers: {
