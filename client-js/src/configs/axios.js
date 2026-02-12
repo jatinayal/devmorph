33bb08039c556aba5876b42e727f4db03bd19ff9
jatinayal <jatinsingh89234@gmail.com>
2026-02-13 00:23:44 +0530

    changed server domain

--- client-js/src/configs/axios.js
+++ client-js/src/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASEURL || 'https://devmorph-server.onrender.com',
+    baseURL: import.meta.env.VITE_BASEURL || 'https://devmorphs.onrender.com',
     withCredentials: true
 })
 
